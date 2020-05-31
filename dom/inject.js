831486cff537d8dc03f0c816c7210bc30598d765
Katherine Kim <katherinekim88@gmail.com>
2020-05-31 15:12:48 -0500

    completed html insertion

--- dom/inject.js
+++ dom/inject.js
@@ -1,7 +1,7 @@
 /*Exercise instructions : take the html code from the following codepen : https://codepen.io/ayunas-the-scripter/pen/WNQgpqB, and use JavaScript DOM manipulation to inject the html into the inject id element of inject.html
 */
 
-let paraInject = document.createElement("p");
+/* let paraInject = document.createElement("p");
 paraInject.innerHTML = <center>
 <h1><i>HTML Practice Exercise TEKcamp.</i></h1>'
 <h2><a href="">TEKsystems "TEKcamp"</a></h2>
@@ -18,4 +18,6 @@ paraInject.innerHTML = <center>
 <p>My instructor’s email address is: <a href="mailto:ayunas@teksystems.com">ayunas@teksystems.com</a>.</p>
 <h1>Have a great day!</h1>  I really look forward to learning how to code!  Have a great day."  
 -[Team "Git'er Done"]
-document.getElementById("inject").appendChild(paraInject);
+document.getElementById("inject").appendChild(paraInject); */
+
+document.getElementById("inject").innerHTML = '<center><h1><i>HTML Practice Exercise TEKcamp.</i></h1><h2><a href="">TEKsystems "TEKcamp"</a></h2></center><hr /><p>I love <i>HTML</i> for the following reasons:</p><ol><li>I learned it quickly.</li><li>I can make web pages using code<li> It’s fun.</li></ol><hr /><p>My instructor’s email address is: <a href="mailto:ayunas@teksystems.com">ayunas@teksystems.com</a>.</p><h1>Have a great day!</h1>  I really look forward to learning how to code!  Have a great day.  -[Team "Giter Done"]'
